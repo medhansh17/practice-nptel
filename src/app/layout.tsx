fed78913c0a00d42fb2cba424ffc73b73405682a
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2024-04-19 04:21:25 +0530

    chore: stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${"G-C89F3H5QQV"});
+              gtag('config', "G-C89F3H5QQV");
           `}
         </Script>
       </head>
