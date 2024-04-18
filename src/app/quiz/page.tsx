cec4b37915cba5755847cfbf25e3f94198e9faa1
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2024-04-19 00:23:28 +0530

    complete startQuiz ppage and init quiz page

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Quiz(){
+
+    const searchParams = useSearchParams()
+ 
+    const courseId = searchParams.get('courseId')
+    const mode = searchParams.get('mode')
+    const syllabus = searchParams.get('syllabus')
+    const router = useRouter();
+
+    useEffect(() => {
+
+        console.log(courseId, mode, syllabus);  
+        if(mode === undefined || syllabus === undefined || courseId === undefined){
+            router.replace("/");
+        }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [mode, syllabus, courseId]);
+
+    return (
+        <div>
+            <h1>Quiz {courseId}</h1>
+        </div>
+    );
+
+}
--- src/app/startQuiz/page.tsx
+++ src/app/startQuiz/page.tsx
@@ -1,29 +1,66 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { useRouter } from "next/navigation";
 import { courses } from "@/utils/course";
-import { useState } from "react";
-import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
+interface courseType {
+  id: number;
+  name: string;
+  totalModules: number;
+}
 
 export default function StartQuiz() {
-  const params = useParams();
-  const courseId = params.courseId;
+  const searchParams = useSearchParams();
+
+  const courseId = searchParams.get("courseId");
+  // const courseId = 1;
   const [mode, setMode] = useState("test");
   const [syllabusOption, setSyllabusOption] = useState("full");
+  const [course, setCourse] = useState<courseType>();
+  const router = useRouter();
 
-  const handleSubmission: (e: any) => void = (e: any) => {
+  const handleSubmission = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(e);
+    const formData = new FormData(e.currentTarget);
+    const mode = formData.get("mode");
+    const syllabus = formData.get("syllabus");
+    router.push(`/quiz?courseId=${courseId}&mode=${mode}&syllabus=${syllabus}`);
+    // console.log(`/quiz?courseId=${courseId}&mode=${mode}&syllabus=${syllabus}`);
   };
+
+  useEffect(() => {
+    const courseFilter = courses.filter(
+      (course) => course.id === Number(courseId)
+    )[0];
+    setCourse(courseFilter);
+    console.log(courseFilter);
+  }, [courseId]);
+
   return (
     <div className="h-screen flex justify-center items-center">
-      <form id="form" action={(e) => handleSubmission(e)}>
-        <input type="hidden" name="courseId" value={params.courseId} />
+      {courseId && <form
+        id="form"
+        className="flex flex-col gap-4 w-full max-w-[400px]"
+        action="/some-action"
+        onSubmit={handleSubmission}
+      >
+        {course && (
+          <h1 className="w-full max-w-[400px] text-[1.5rem] font-semibold text-center mb-10">
+            {course?.name}
+          </h1>
+        )}
+        <input type="hidden" name="courseId" value={courseId} />
         <input type="hidden" name="syllabusOption" value={syllabusOption} />
         <label htmlFor="mode">Mode:</label>
         <select
           id="mode"
           name="mode"
+          className="text-black p-2 rounded-md cursor-hover"
           value={mode}
+          required
+          defaultValue={"test"}
           onChange={(e) => setMode(e.target.value)}
         >
           <option value="test">Test</option>
@@ -34,13 +71,26 @@ export default function StartQuiz() {
           id="syllabus"
           name="syllabus"
           value={syllabusOption}
+          defaultValue={"full"}
+          className="text-black p-2 rounded-md cursor-hover"
+          required
           onChange={(e) => setSyllabusOption(e.target.value)}
         >
           <option value="full">Full</option>
-          {}
+          {course &&
+            Array.from({ length: Number(course.totalModules) }, (_, index) => (
+              <option key={index + 1} value={index + 1}>
+                Module {index + 1}
+              </option>
+            ))}
         </select>
-        <button type="submit">Start Quiz</button>
-      </form>
+        <button
+          className="p-2 px-4 my-4 border rounded-md hover:bg-gray-800"
+          type="submit"
+        >
+          Start Quiz
+        </button>
+      </form>}
     </div>
   );
 }
