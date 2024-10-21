d86d485ded1ddce3812242b414845c2b33cd408f
Medhansh Jain <medhanshjain2010@gmail.com>
2024-10-21 11:13:18 +0530

    styles: add some love

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <div className="fixed bottom-0 text-center w-full border-t border-gray-200 bg-gray h-[30px]">
-      Made with &hearts; by{" "}
-      <a href="https://linkedin.com/in/rupaaksrinivas" target="_blank">
+      Made with ❤️ by{" "}
+      <a href="https://linkedin.com/in/rupaaksrinivas" target="_blank" className=" underline">
         {" "}
         Rupaak
       </a>{" "}
       and{" "}
-      <a href="https://www.linkedin.com/in/medhansh-jain/" target="_blank">
+      <a href="https://www.linkedin.com/in/medhansh-jain/" target="_blank" className="underline">
         {" "}
         Medhansh{" "}
       </a>
